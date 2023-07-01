6c9bce4922e003a759e72e8ea3e4038bbad72bbe
MD Nurul Haque Siddiki <3nurulhaque3645@gmail.com>
2023-07-01 18:00:54 +0600

    Features section added

--- src/LandingPage/Feature/Features.jsx
+++ src/LandingPage/Feature/Features.jsx
@@ -1,35 +1,115 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Common/SectionTitle/SectionTitle';
-import { FaCode } from "react-icons/fa";
+import { FaCode, FaLaptopCode, FaWordpressSimple } from "react-icons/fa";
+import { AiOutlineCode, AiOutlineRedo, AiOutlineSetting, AiOutlineShopping,  } from "react-icons/ai";
+import Tilt from 'react-parallax-tilt';
+
+
 const Features = () => {
-
-    // const [services, setServices] = useState([]);
-
-    // useEffect(() => {
-    //     fetch('services.json')
-    //         .then(res => res.json())
-    //         .then(data => setServices(data))
-    // }, [])
-    // // console.log(services);
+    const [scale, setScale] = useState(1.03);
 
     return (
         <div className='py-16 border-b-2 border-[#121415] container mx-auto'>
             <SectionTitle title='What I Do' subTitle='Features'></SectionTitle>
-            <div>
-                {
-                    services.map((service, index) => (
-                        <div className="card w-96 bg-base-200 shadow-xl" key={index}>
-                            <div className="card-body">
-                                <FaCode className='text-primary text-5xl font-light'></FaCode>
-                                <h2 className="card-title">{service.name}</h2>
-                                <p>{service.description}</p>
-                                <div className="card-actions">
-                                    {/* Add necessary actions or buttons here */}
-                                </div>
+            <div className='pt-10 grid md:grid-cols-3 gap-5'>
+
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b from-base-200 h-full cursor-pointer shadow-xl hover:bg-gradient-to-t">
+                        <div className="card-body">
+                            <FaCode className='text-primary text-5xl font-light'></FaCode>
+                            <h2 className="card-title">Web Design</h2>
+                            <p>Create visually appealing and user-friendly websites that captivate visitors and effectively communicate your brand's message.</p>
+                            <div className="card-actions">
+
                             </div>
                         </div>
-                    ))
-                }
+                    </div>
+                </Tilt>
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b hover:bg-gradient-to-t h-full cursor-pointer from-base-200 shadow-xl">
+                        <div className="card-body">
+                            <AiOutlineCode className='text-primary text-5xl font-light'></AiOutlineCode>
+                            <h2 className="card-title">Web Development</h2>
+                            <p>Build custom websites and web applications using the latest technologies to provide seamless functionality and a great user experience.</p>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b hover:bg-gradient-to-t h-full cursor-pointer from-base-200 shadow-xl">
+                        <div className="card-body">
+                            <AiOutlineSetting className='text-primary text-5xl font-light'></AiOutlineSetting>
+                            <h2 className="card-title">Website Maintenance</h2>
+                            <p>Ensure your website remains secure, up-to-date, and performs optimally with regular maintenance and updates.</p>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b hover:bg-gradient-to-t h-full cursor-pointer from-base-200 shadow-xl">
+                        <div className="card-body">
+                            <AiOutlineShopping className='text-primary text-5xl font-light'></AiOutlineShopping>
+                            <h2 className="card-title">E-commerce Website</h2>
+                            <p>Establish a powerful online store with secure payment gateways, intuitive product catalogs, and a smooth shopping experience.</p>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b hover:bg-gradient-to-t h-full cursor-pointer from-base-200 shadow-xl">
+                        <div className="card-body">
+                            <AiOutlineRedo className='text-primary text-5xl font-light'></AiOutlineRedo>
+                            <h2 className="card-title">Website Redesign</h2>
+                            <p>Transform your outdated website into a modern and engaging online presence that aligns with current design trends and user expectations.</p>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+                <Tilt
+                    tiltEnable={false}
+                    scale={scale}
+                    transitionSpeed={2500}
+                >
+                    <div className="card w-full bg-gradient-to-b hover:bg-gradient-to-t h-full cursor-pointer from-base-200 shadow-xl">
+                        <div className="card-body">
+                            <FaWordpressSimple className='text-primary text-5xl font-light'></FaWordpressSimple>
+                            <h2 className="card-title">WordPress Website</h2>
+                            <p>Leverage the power of WordPress to create feature-rich, customizable websites that are easy to manage and scale.</p>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+
             </div>
         </div>
     );
